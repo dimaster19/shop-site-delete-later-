9a2457dbd47790aab016d9d99eb29daa4478f9b1
Admin <Admin@HOME-PC>
2022-12-29 19:30:10 +0300

    push

---
+++ js/dropdown.js
@@ -0,0 +1,47 @@
+
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+  
+//   window.onclick = function(e) {
+//     if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+let intervalId;
+
+document.querySelectorAll('.dropbtn').forEach(e => {
+    e.addEventListener('click', e => {
+        const menu = e.currentTarget.dataset.path;
+        document.querySelectorAll('.dropdown-menu').forEach(e => {
+            if (!document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                e.classList.remove('menu-active');
+                e.classList.remove('open');
+                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active');
+                intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.add('open');
+                }, 0);
+            }
+
+            if (document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                clearTimeout(intervalId);
+                document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active');
+                intervalId = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+                }, 0);
+            }
+
+            window.onclick = e => {
+                if (e.target == document.querySelector(`[data-target=${menu}]`) || e.target == document.querySelector(`[data-path=${menu}]`)) {
+                    return;
+                } else {
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active');
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+                }
+            }
+        });
+    });
+});
